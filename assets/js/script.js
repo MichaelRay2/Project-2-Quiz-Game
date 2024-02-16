6732efc144661d919f9631e055c3c8ec7706dfa9
MichaelRay2 <michaelray2809@yahoo.com>
2024-02-16 09:12:05 +0000

    Added questions to the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+let questions = [
+    {
+        question: "Which is the capital city of Spain?", 
+        answers: [
+            { text: "Barcelona", correct: false},
+            { text: "Madrid", correct: true},
+            { text: "Lisbon", correct: false},
+            { text: "Sevilla", correct: false},
+            { text: "Ibiza", correct: false},
+            { text: "Valencia", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of Australia?", 
+        answers: [
+            { text: "Sydney", correct: false},
+            { text: "Melbourne", correct: false},
+            { text: "Brisbane", correct: false},
+            { text: "Wellington", correct: false},
+            { text: "Gold Coast", correct: false},
+            { text: "Canberra", correct: true},
+        ]
+    },
+    {
+        question: "Which is the capital city of Nigeria?", 
+        answers: [
+            { text: "Abuja", correct: true},
+            { text: "Accra", correct: false},
+            { text: "Durban", correct: false},
+            { text: "Lagos", correct: false},
+            { text: "Tripoli", correct: false},
+            { text: "Kinshasa", correct: false},
+        ]
+    },
+    {
+        question: "Which player was the youngest player to have scored a premier league goal?", 
+        answers: [
+            { text: "Wayne Rooney", correct: false},
+            { text: "Frank Lampard", correct: false},
+            { text: "Raheem Sterling", correct: false},
+            { text: "James Milner", correct: false},
+            { text: "James Vaughan", correct: true},
+            { text: "Cesc Fabregas", correct: false},
+        ]
+    },
+    {
+        question: "As of the beginning of 2024, who is Newcastle United's highest transfer sale?", 
+        answers: [
+            { text: "Ayoze Perez", correct: false},
+            { text: "Andy Carroll", correct: true},
+            { text: "Mousa Sissoko", correct: false},
+            { text: "Yohan Cabaye", correct: false},
+            { text: "Jonathan Woodgate", correct: false},
+            { text: "Georginio Wijnaldum", correct: false},
+        ]
+    },
+    {
+        question: "Which of these players has scored the fastest hatrick in premier league history?", 
+        answers: [
+            { text: "Dwight Yorke", correct: false},
+            { text: "Sadio Mane", correct: true},
+            { text: "Kevin De Bruyne", correct: false},
+            { text: "Jermaine Pennant", correct: false},
+            { text: "Yannick Bolasie", correct: false},
+            { text: "Michael Owen", correct: false},
+        ]
+    },
+    {
+        question: "Who is Tottenham Hotspur's most expensive signing?", 
+        answers: [
+            { text: "Christian Romero", correct: false},
+            { text: "Brennan Johnson", correct: false},
+            { text: "James Maddison", correct: false},
+            { text: "Davinson Sanchez", correct: false},
+            { text: "Richarlison", correct: false},
+            { text: "Tanguy Ndombele", correct: true},
+        ]
+    }
+]
